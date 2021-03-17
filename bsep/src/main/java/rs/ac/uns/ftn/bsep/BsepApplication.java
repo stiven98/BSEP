327c85bbd0e3a5544ede4150fe799348196e3616
Aca Simic <acasimic98@uns.ac.rs>
2021-03-17 10:33:21 +0100

    Init project

---
+++ bsep/src/main/java/rs/ac/uns/ftn/bsep/BsepApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.bsep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsepApplication.class, args);
+	}
+
+}
