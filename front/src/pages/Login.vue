de21bffa0ccb1243c00e16b8e8b861ee41bf67d4
stojke13 <stojakovic_djordje@yahoo.com>
2021-03-21 13:58:37 +0100

    added routes

---
+++ front/src/pages/Login.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <div style="max-width:400px">
+        <q-form
+      @submit="onSubmit"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="username"
+        label="Your username *"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+      />
+
+      <q-input
+        filled
+        v-model="pass"
+        label="Your password"
+        lazy-rules
+       :rules="[ val => val && val.length > 0 || 'Please type something']"
+      />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+      </div>
+    </q-form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data: function () {
+    return {
+      username: '',
+      pass: ''
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.username === 'admin' && this.pass === 'admin') {
+        this.$router.push('/adminHome')
+      }
+    }
+  }
+}
+</script>
--- front/src/router/routes.js
+++ front/src/router/routes.js
@@ -2,9 +2,23 @@
 const routes = [
   {
     path: '/',
+    component: () => import('pages/Login.vue'),
+    children: [
+      { path: '', component: () => import('pages/Login.vue') }
+    ]
+  },
+  {
+    path: '/adminHome',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      // { path: '', component: () => import('pages/AddCertificate.vue') }
+    ]
+  },
+  {
+    path: '/newCertificate',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/AddCertificate.vue') }
     ]
   },
 
