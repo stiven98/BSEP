fc5f814c6d8a9d2f5b69b9fe2fbcbc10d95f6b43
Jovan Bosnic <jovan.bosnic_7@uns.ac.rs>
2021-04-23 18:55:15 +0200

    Code refactor

--- bsep/src/main/java/rs/ac/uns/ftn/bsep/controller/UserController.java
+++ bsep/src/main/java/rs/ac/uns/ftn/bsep/controller/UserController.java
@@ -4,26 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import rs.ac.uns.ftn.bsep.domain.dto.LoginDTO;
 import rs.ac.uns.ftn.bsep.domain.dto.LoginResponseDTO;
 import rs.ac.uns.ftn.bsep.domain.dto.RegisterUserDTO;
 import rs.ac.uns.ftn.bsep.domain.dto.ResetPasswordDTO;
-import rs.ac.uns.ftn.bsep.domain.users.Admin;
-import rs.ac.uns.ftn.bsep.domain.users.Authority;
 import rs.ac.uns.ftn.bsep.domain.users.User;
-import rs.ac.uns.ftn.bsep.email.EmailSender;
-import rs.ac.uns.ftn.bsep.security.TokenUtils;
 import rs.ac.uns.ftn.bsep.service.UserService;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.websocket.server.PathParam;
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.UUID;
 
 @RestController
@@ -32,28 +21,10 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private TokenUtils tokenUtils;
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> createAuthenticationToken(@RequestBody LoginDTO dto, HttpServletResponse response) {
-        Authentication authentication = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(dto.getUsername(),
-                        dto.getPassword()));
-        // Ubaci korisnika u trenutni security kontekst
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        // Kreiraj token za tog korisnika
-        User user = (User) authentication.getPrincipal();
-        String jwt = tokenUtils.generateToken(user.getUsername());
-        int expiresIn = tokenUtils.getExpiredIn();
-        List<Authority> authorities = new ArrayList<>();
-        user.getAuthorities().stream().forEach(a -> authorities.add((Authority) a));
-        // Vrati token kao odgovor na uspesnu autentifikaciju
-        LoginResponseDTO responseDTO= new LoginResponseDTO(user.getUsername(),jwt,authorities);
-        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    public ResponseEntity<LoginResponseDTO> createAuthenticationToken(@RequestBody LoginDTO dto) {
+        return new ResponseEntity<>(userService.login(dto), HttpStatus.OK);
     }
 
     @PostMapping("/forgotPassword")
